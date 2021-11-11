9c25eefd546d1218031dacdea0f5232efa9dbf8a
Aaron Pop <FezFamiliar@yahoo.com>
2021-11-11 10:07:18 +0200

    Update intcode_computer.py

--- aoc/2019/intcode_computer.py
+++ aoc/2019/intcode_computer.py
@@ -1,7 +1,24 @@
 
-# modes:
-# 0 - position mode
-# 1 - immediate mode
+# parameter modes:
+# 0 - position mode (take the value at position x)
+# 1 - immediate mode (just take the value x)
+
+# instructions:
+# 1 - add (num1, num2, store_value), 3 params
+# 2 - multiply (num1, num2, store_value) 3 params
+# 3 - take an input and save it to its only parameter (store_value) 1 param
+# 4 - out the value at its only parameter (store_value) 1 param
+# 99 - break () 0 params
+
+
+# additional notes:
+# 1. Encountering an unknown opcode means something went wrong.
+# 2. The values used immediately after an opcode, if any, are called the instruction's parameters
+# 3. The address of the current instruction is called the instruction pointer; it starts at 0. After an instruction finishes, 
+#    the instruction pointer increases by the number of values in the instruction; 
+#    until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters)
+# 4. Parameter modes are stored in the same value as the instruction's opcode.
+# 5. The instruction pointer should increase by the number of values in the instruction after the instruction finishes. (step value)
 
 class IntcodeComputer:
     def __init__(self, input, mode, noun = '', verb = ''):
