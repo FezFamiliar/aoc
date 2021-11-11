978666f40e7e98dfbf9ece377659741e9f96bbb8
Aaron Pop <FezFamiliar@yahoo.com>
2021-11-11 16:27:12 +0200

    Update intcode_computer.py

--- aoc/2019/intcode_computer.py
+++ aoc/2019/intcode_computer.py
@@ -21,12 +21,11 @@
 # 5. The instruction pointer should increase by the number of values in the instruction after the instruction finishes. (step value)
 
 class IntcodeComputer:
-    def __init__(self, input, mode, noun = '', verb = ''):
+    def __init__(self, input, noun = '', verb = ''):
         self.input = input
         self.input = self.input.split(',')
         self.input = [int(x) for x in self.input]
         self.n = len(self.input)
-        self.mode = mode
         self.instructions = [1, 2, 99]
         if noun != '':
             self.input[1] = noun
@@ -34,7 +33,8 @@ class IntcodeComputer:
             self.input[2] = verb
             
     def run(self):
-        for i in range(0, self.n, 4):
+        i = 0
+        while i < self.n:
     
             opcode = self.input[i]
         
@@ -53,12 +53,14 @@ class IntcodeComputer:
             
             
             self.input[pos3] = val
+            
+            i += 4
     
     def __eq__(self, other):
-        return self.input == other.input and self.mode == other.mode
+        return self.input == other.input
         
     def __repr__(self):
-        return "<input:%s mode:%s length:%s instructions:%s noun:%s verb:%s>" % (self.input, self.mode, self.n, self.instructions, self.input[1], self.input[2])
+        return "<input:%s length:%s instructions:%s noun:%s verb:%s>" % (self.input, self.n, self.instructions, self.input[1], self.input[2])
 
 
     
