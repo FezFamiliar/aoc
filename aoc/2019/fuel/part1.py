04d69aaa26047d5ff05ff44e89faefb7dda12780
Aaron Pop <FezFamiliar@yahoo.com>
2022-03-21 23:06:37 +0200

    Update part1.py

--- aoc/2019/fuel/part1.py
+++ aoc/2019/fuel/part1.py
@@ -7,6 +7,7 @@ total_ores_spent = 0
 all_chemicals = {}
 leftover_chemicals = {}
 acquired = {}
+ores = {}
 
 def printChemicalStatus():
     
@@ -27,27 +28,73 @@ def printChemicalStatus():
 
 
 
-def consume(chemicals):
+def IsInCache(c):
+    for k, v in leftover_chemicals.items():
+        if c[1] == k and v >= int(c[0]):
+            return True
+    
+    return False
+
+def checkOre(c):
     global total_ores_spent
+    print(f"this is it: {c}")
+    for k, v in ores.items():
+        if k[1] == c[1]:
 
+            if int(c[0]) <= int(k[0]):
+                if IsInCache(c):
+                    acquired[c[1]] += int(leftover_chemicals[c[1]])              # aquire chemical from cache
+                    leftover_chemicals[c[1]] -= int(c[0])                                                  # decrease number in cache
+                else:
+                    
+                    if c[1] in acquired:
+                        acquired[c[1]] += int(c[0])
+                    else:
+                        acquired[c[1]] = int(c[0])
+                    total_ores_spent += int(v[0])      # increment total_ores_spent
+                    l = int(k[0]) - int(c[0])          # calculate leftover chemicals
+
+                    if c[1] in leftover_chemicals:
+                        leftover_chemicals[c[1]] += l 
+                    else:
+                        leftover_chemicals[c[1]] = l       # save leftover chemicals in chace
+                #printChemicalStatus()   
+            else:
+                print(f"im here")
+
+
+                aux = 0
+                while int(c[0]) > aux:
+
+                    total_ores_spent += int(v[0])
+                    aux += int(k[0])
+                if c[1] in acquired:
+                    acquired[c[1]] += int(c[0])
+                else:
+                    acquired[c[1]] = int(c[0])
+
+                if c[1] in leftover_chemicals:
+                    leftover_chemicals[c[1]] += aux % int(c[0])
+                else:
+                    leftover_chemicals[c[1]] = aux % int(c[0])       # save leftover chemicals in chace
+                #exit(1)
+            return True
+    
+    return False
+
+
+
+def consume(chemicals):  # chemicals needed for FUEL
+    global total_ores_spent
     for c in chemicals:
-        for k, v in all_chemicals.items():
-            if c[1] == k[1]:                
-                if type(v) == list:
-                    for i in v:
-                        v[1]
-                        print(f"k - {i}") 
-                else:                
-                    if int(c[0]) < int(k[0]):            # check quantity & buy
-                        if v[1] == 'ORE':                # check if its directly ORE, if not, somehow recurse
+        if checkOre(c):
+            continue
+        else:
+            for k, v in all_chemicals.items():
+                if c[1] == k[1]:           
+                    consume(v)
+                    acquired[c[1]] = int(c[0])
 
-                            acquired[c[1]] = int(c[0]) 
-                            if not checkLeftovers(c):
-                                total_ores_spent += int(v[0])
-
-                            leftover_chemicals[c[1]] = int(k[0]) % int(c[0])
-                        else:
-                            consume()
 
 
 def checkLeftovers(chemical_tuple):
@@ -61,12 +108,13 @@ def checkLeftovers(chemical_tuple):
 
 
 
-input = '''10 ORE => 10 A
-1 ORE => 1 B
-7 A, 1 B => 1 C
-7 A, 1 C => 1 D
-7 A, 1 D => 1 E
-7 A, 1 E => 1 FUEL'''
+input = '''9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL'''
 
 input = input.split('\n')
 
@@ -91,16 +139,17 @@ for i in input:
         all_chemicals[value_tuple] = key_tuple
 
 
-print(f"All chemical reactions: (k, v)")
-
-
 for k, v in all_chemicals.items():
-    print(f"{k} => {v}")
+
+    print(f"{k} --- {v}")
+    if v[1] == 'ORE':
+        ores[k] = v
     if k[1] == 'FUEL':
         print(f"Chemicals needed for fuel: {v}")
         consume(v)
-        #printChemicalStatus()
+        printChemicalStatus()
         break
+
 
 
 
